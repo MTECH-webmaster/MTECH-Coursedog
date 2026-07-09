85aee13e8ad89d747fe720e1e577bb4ee98a07c9
MTECH <m02141@AD.MTEC.EDU>
2026-07-09 10:13:35 -0600

    readme change

---
+++ mtech-coursedog.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Plugin Name: MTECH Coursedog
+ * Description: MTECH website integration with Coursedog Curriculum
+ * Version: 1.0
+ * Author: MTECH
+ * Author URI: https://mtec.edu
+ */
