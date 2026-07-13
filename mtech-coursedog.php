c833eabc932a1099afe3d3ab77f3a8e1ed204104
MTECH <webmaster@mtec.edu>
2026-07-13 16:57:03 -0600

    Plugin init function added

--- mtech-coursedog.php
+++ mtech-coursedog.php
@@ -60,4 +60,15 @@ Class MTECH_Coursedog {
         <?php>
     }
 
+    register_activation_hook(__FILE__, array('Coursedog_API', 'activate_plugin'));
+    register_deactivation_hook(__FILE__, array('Coursedog_API', 'deactivate_plugin'));
+
+    // Initialize the plugin
+    function coursedog_api_init() {
+        return Coursedog_API::get_instance();
+    }
+
+    // Start the plugin
+    $coursedog_api = coursedog_api_init();
+
 }
