a5bc09a161183080dbcb8b503929e1c18a919289
MTECH <webmaster@mtec.edu>
2026-07-13 16:46:01 -0600

    Constructor, init function

--- mtech-coursedog.php
+++ mtech-coursedog.php
@@ -6,3 +6,38 @@
  * Author: MTECH
  * Author URI: https://mtec.edu
  */
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+Class MTECH_Coursedog {
+
+    private static $instance = null;
+
+    public static function get_instance() {
+        if (null === self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function __construct() {
+        // Initialize the plugin
+        add_action('plugins_loaded', array($this, 'init'));
+    }
+
+    public static function activate_plugin() {
+        // Items for activation
+    }
+
+    public static function deactivate_plugin() {
+        // Items for deactivation
+    }
+
+    public function init() {
+        // Init
+    }
+
+}
