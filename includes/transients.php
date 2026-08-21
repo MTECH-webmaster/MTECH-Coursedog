1812c510d81a52869006f56c485c5c14cff31360
MTECH <webmaster@mtec.edu>
2026-08-21 10:55:45 -0600

    Add test logs for cache verification

--- includes/transients.php
+++ includes/transients.php
@@ -13,10 +13,12 @@ if (!defined('ABSPATH')) {
 function mtech_coursedog_get_program_blob_transient($program_slug) {
     $blob_transient_name = $program_slug . "_blob";
     $transient = get_transient($blob_transient_name);
+    mtech_coursedog_log("get_program_blob_transient function ran for " . $program_slug);
     return $transient;
 }
 
 function mtech_coursedog_set_program_blob_transient($program_slug, $blob_program_data) {
     $blob_transient_name = $program_slug . "_blob";
     set_transient($blob_transient_name, $blob_program_data, 7200);
+    mtech_coursedog_log("set_program_blob_transient function ran for " . $program_slug);
 }
